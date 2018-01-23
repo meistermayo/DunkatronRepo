1e34c843c98e4f69bd4cf3d70fdd23f94b7a64c2
Luke Desktop <meistermayo@gmail.com>
2018-01-23 12:53:30 -0500

    Sprites
    Rocket Launcher Sprite faces right
    Better Leech gun Sprite added
    
    attempted to change resolution (failed)

---
+++ ShootsIdeas/Assets/ScaleWithScreenSize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleWithScreenSize : MonoBehaviour {
+
+	[SerializeField] float wRef = 1920f;
+
+	void Update()
+	{
+		
+	}
+
+
+}
